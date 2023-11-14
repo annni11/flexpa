b212d1dedead6538196c0fca5f53e34bafe9787d
annni11 <ann.j.ni@gmail.com>
2023-11-14 11:06:26 -0500

    fixed some es lint

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,9 +1,3 @@
-import Profile from './PatientAccess';
-import EOB from './EOB';
-import App from '../App';
-import Nav from '../components/NavBar';
-import { Routes, Route, Outlet } from 'react-router-dom';
-
 function Home() {
   return (
     <div className='Home'>
